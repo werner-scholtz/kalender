0d175a1bda95f8a4807c86a29cbaff20d6f061b8
werner-scholtz <werner.scholtz@kdab.com>
2023-08-11 11:31:24 +0200

    Added notifylisteners to CalendarController

--- lib/src/models/calendar/calendar_controller.dart
+++ lib/src/models/calendar/calendar_controller.dart
@@ -87,6 +87,7 @@ class CalendarController<T> with ChangeNotifier {
       duration: duration ?? const Duration(milliseconds: 300),
       curve: curve ?? Curves.easeInOut,
     );
+    notifyListeners();
   }
 
   /// Jumps to the [page].
@@ -98,6 +99,7 @@ class CalendarController<T> with ChangeNotifier {
       'Please attach the $CalendarController to a view.',
     );
     _state?.pageController.jumpToPage(page);
+    notifyListeners();
   }
 
   /// Jumps to the [date].
@@ -121,6 +123,7 @@ class CalendarController<T> with ChangeNotifier {
         _state!.adjustedDateTimeRange.start,
       ),
     );
+    notifyListeners();
   }
 
   /// Animates to the [DateTime] provided.
@@ -152,6 +155,7 @@ class CalendarController<T> with ChangeNotifier {
       duration: duration ?? const Duration(milliseconds: 300),
       curve: curve ?? Curves.easeInOut,
     );
+    notifyListeners();
   }
 
   /// Changes the [heightPerMinute] of the view.
@@ -168,11 +172,15 @@ class CalendarController<T> with ChangeNotifier {
       'Please attach the $CalendarController to a $SingleDayView or $MultiDayView.',
     );
     _state?.heightPerMinute?.value = heightPerMinute;
+    notifyListeners();
   }
 
   /// Animates to the [CalendarEvent].
-  Future<void> animateToEvent(CalendarEvent<T> event,
-      {Duration? duration, Curve? curve,}) async {
+  Future<void> animateToEvent(
+    CalendarEvent<T> event, {
+    Duration? duration,
+    Curve? curve,
+  }) async {
     // First animate to the date of the event.
     await animateToDate(
       event.dateTimeRange.start,
@@ -190,5 +198,6 @@ class CalendarController<T> with ChangeNotifier {
         curve: curve ?? Curves.ease,
       );
     }
+    notifyListeners();
   }
 }
