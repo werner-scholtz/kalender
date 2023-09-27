97639f24b130258cd3859cb5657871dfa43bb963
werner-scholtz <werner.scholtz@kdab.com>
2023-09-27 09:14:01 +0200

    updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,4 @@
-import 'dart:io';
-
-import 'package:example/models/event.dart';
-import 'package:example/functions/generate_calendar_events.dart';
-import 'package:example/screens/desktop.dart';
-import 'package:example/screens/mobile.dart';
 import 'package:example/theme/theme.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kalender/kalender.dart';
 
@@ -41,36 +34,127 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late final CalendarEventsController<Event> eventController;
+  final CalendarController<Event> controller = CalendarController();
+  final CalendarEventsController<Event> eventController =
+      CalendarEventsController<Event>();
 
   @override
   void initState() {
     super.initState();
-    eventController = CalendarEventsController<Event>();
-    eventController.addEvents(generateCalendarEvents());
+    DateTime now = DateTime.now();
+    eventController.addEvents([
+      CalendarEvent(
+        dateTimeRange: DateTimeRange(
+          start: now,
+          end: now.add(const Duration(hours: 1)),
+        ),
+      ),
+      CalendarEvent(
+        dateTimeRange: DateTimeRange(
+          start: now.add(const Duration(hours: 2)),
+          end: now.add(const Duration(hours: 5)),
+        ),
+      ),
+      CalendarEvent(
+        dateTimeRange: DateTimeRange(
+          start: now,
+          end: now.add(const Duration(days: 1)),
+        ),
+      ),
+    ]);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          if (kIsWeb || !(Platform.isAndroid || Platform.isIOS)) {
-            if (constraints.maxWidth < 500) {
-              return MobileScreen(
-                eventsController: eventController,
-              );
-            }
-            return DesktopScreen(
-              eventsController: eventController,
-            );
-          } else {
-            return MobileScreen(
-              eventsController: eventController,
-            );
-          }
-        },
+      body: CalendarView<Event>(
+        controller: controller,
+        eventsController: eventController,
+        viewConfiguration: const WeekConfiguration(),
+        tileBuilder: _tileBuilder,
+        multiDayTileBuilder: _multiDayTileBuilder,
+        scheduleTileBuilder: _scheduleTileBuilder,
       ),
     );
   }
+
+  Widget _tileBuilder(
+    CalendarEvent<Event> event,
+    TileConfiguration configuration,
+  ) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: _getTileColor(
+          event.eventData?.color ?? Colors.blue,
+          configuration.tileType,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: configuration.tileType != TileType.ghost
+            ? Text(event.eventData?.title ?? 'New Event')
+            : null,
+      ),
+    );
+  }
+
+  Widget _multiDayTileBuilder(
+    CalendarEvent<Event> event,
+    MultiDayTileConfiguration configuration,
+  ) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: _getTileColor(
+          event.eventData?.color ?? Colors.blue,
+          configuration.tileType,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: configuration.tileType != TileType.ghost
+            ? Text(event.eventData?.title ?? 'New Event')
+            : null,
+      ),
+    );
+  }
+
+  Widget _scheduleTileBuilder(CalendarEvent<Event> event, DateTime date) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        color: event.eventData?.color ?? Colors.blue,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Text(event.eventData?.title ?? 'New Event'),
+    );
+  }
+
+  Color _getTileColor(Color color, TileType tileType) {
+    switch (tileType) {
+      case TileType.normal:
+        return color.withAlpha(200);
+      case TileType.selected:
+        return color;
+      case TileType.ghost:
+        return color.withAlpha(100);
+      default:
+        return color;
+    }
+  }
+}
+
+class Event {
+  Event({
+    required this.title,
+    this.description,
+    this.color,
+  });
+
+  /// The title of the [Event].
+  final String title;
+
+  /// The description of the [Event].
+  final String? description;
+
+  /// The color of the [Event] tile.
+  final Color? color;
 }
