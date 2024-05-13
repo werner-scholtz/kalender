6f74e6450748ed42fee480033fc3eab8649ae7a7
werner-scholtz <werner.scholtz@kdab.com>
2024-05-13 08:35:08 +0200

    example cleanup

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' hide ViewConfiguration;
 import 'package:kalender/kalender.dart';
 
 void main() {
