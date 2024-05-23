af1fc779814623bd913e59cbe3323790d50aa793
werner-scholtz <werner.scholtz@kdab.com>
2024-05-23 15:12:22 +0200

    Fixed test import

--- test/view_configuration_test.dart
+++ test/view_configuration_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kalender/kalender.dart';
 import 'package:kalender/src/extensions.dart';
-import 'package:kalender/src/models/view_configurations/export.dart';
 
 void main() {
   final start = DateTime(2024);
