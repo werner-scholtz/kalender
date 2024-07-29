dd80647bcd7dbe81c2d0a8fd9e90ea5a03d41358
werner-scholtz <werner.scholtz@kdab.com>
2024-07-29 17:00:24 +0200

    Cleanup extensions

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -277,57 +277,32 @@ extension DateTimeExtensions on DateTime {
     return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
   }
 
-  String get dayNameEnglish {
-    switch (weekday) {
-      case 1:
-        return 'Monday';
-      case 2:
-        return 'Tuesday';
-      case 3:
-        return 'Wednesday';
-      case 4:
-        return 'Thursday';
-      case 5:
-        return 'Friday';
-      case 6:
-        return 'Saturday';
-      case 7:
-        return 'Sunday';
-      default:
-        return '';
-    }
-  }
+  String get dayNameEnglish => switch (weekday) {
+        1 => 'Monday',
+        2 => 'Tuesday',
+        3 => 'Wednesday',
+        4 => 'Thursday',
+        5 => 'Friday',
+        6 => 'Saturday',
+        7 => 'Sunday',
+        _ => throw Exception('Invalid weekday'),
+      };
 
-  String get monthNameEnglish {
-    switch (month) {
-      case 1:
-        return 'January';
-      case 2:
-        return 'February';
-      case 3:
-        return 'March';
-      case 4:
-        return 'April';
-      case 5:
-        return 'May';
-      case 6:
-        return 'June';
-      case 7:
-        return 'July';
-      case 8:
-        return 'August';
-      case 9:
-        return 'September';
-      case 10:
-        return 'October';
-      case 11:
-        return 'November';
-      case 12:
-        return 'December';
-      default:
-        return '';
-    }
-  }
+  String get monthNameEnglish => switch (month) {
+        1 => 'January',
+        2 => 'February',
+        3 => 'March',
+        4 => 'April',
+        5 => 'May',
+        6 => 'June',
+        7 => 'July',
+        8 => 'August',
+        9 => 'September',
+        10 => 'October',
+        11 => 'November',
+        12 => 'December',
+        _ => throw Exception('Invalid month'),
+      };
 }
 
 extension TimeOfDayExtension on TimeOfDay {
