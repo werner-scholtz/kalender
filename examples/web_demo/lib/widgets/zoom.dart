efa1324483b65f900e431c823e4525d30572c8d4
werner-scholtz <werner.scholtz@kdab.com>
2024-09-05 10:49:33 +0200

    Calender zoom focus when cursor enters mouse region.
    - Request focus when the cursor enters the CalendarZoom widget.

--- examples/web_demo/lib/widgets/zoom.dart
+++ examples/web_demo/lib/widgets/zoom.dart
@@ -29,45 +29,50 @@ class _CalendarZoomDetectorState extends State<CalendarZoomDetector> {
   ValueNotifier<bool> isCtrlPressed = ValueNotifier(false);
   double _pointerYOffset = 0;
 
+  final _focus = FocusNode();
+
   @override
   Widget build(BuildContext context) {
-    return Listener(
-      onPointerSignal: (event) {
-        if (event is! PointerScrollEvent || !isCtrlPressed.value) return;
-        const zoomSpeed = 0.1;
-        final height = heightPerMinute?.value;
-        if (height == null) return;
+    return MouseRegion(
+      onEnter: (event) => _focus.requestFocus(),
+      child: Listener(
+        onPointerSignal: (event) {
+          if (event is! PointerScrollEvent || !isCtrlPressed.value) return;
+          const zoomSpeed = 0.1;
+          final height = heightPerMinute?.value;
+          if (height == null) return;
 
-        var newHeight = height + (event.scrollDelta.direction.sign * -1) * zoomSpeed;
-        newHeight = newHeight.clamp(0.5, 2);
+          var newHeight = height + (event.scrollDelta.direction.sign * -1) * zoomSpeed;
+          newHeight = newHeight.clamp(0.5, 2);
 
-        final zoomRatio = newHeight / height;
-        final scrollPosition = scrollController?.position.pixels;
-        if (scrollPosition == null) return;
+          final zoomRatio = newHeight / height;
+          final scrollPosition = scrollController?.position.pixels;
+          if (scrollPosition == null) return;
 
-        final pointerPosition = scrollPosition + _pointerYOffset;
-        final newPosition = (pointerPosition * zoomRatio) - _pointerYOffset;
-        scrollController?.jumpTo(newPosition);
-        heightPerMinute?.value = newHeight;
-      },
-      onPointerHover: (event) => _pointerYOffset = event.localPosition.dy,
-      behavior: HitTestBehavior.translucent,
-      child: KeyboardListener(
-        autofocus: true,
-        focusNode: FocusNode(),
-        onKeyEvent: (KeyEvent event) {
-          final key = event.logicalKey;
-          if (key != LogicalKeyboardKey.controlLeft && key != LogicalKeyboardKey.controlRight) {
-            return;
-          }
-          if (event is KeyDownEvent) isCtrlPressed.value = true;
-          if (event is KeyUpEvent) isCtrlPressed.value = false;
+          final pointerPosition = scrollPosition + _pointerYOffset;
+          final newPosition = (pointerPosition * zoomRatio) - _pointerYOffset;
+          scrollController?.jumpTo(newPosition);
+          heightPerMinute?.value = newHeight;
         },
-        child: ValueListenableBuilder(
-          valueListenable: isCtrlPressed,
-          builder: (context, value, _) => ScrollConfiguration(
-            behavior: value ? const ScrollBehaviorNever() : const MaterialScrollBehavior(),
-            child: widget.child,
+        onPointerHover: (event) => _pointerYOffset = event.localPosition.dy,
+        behavior: HitTestBehavior.translucent,
+        child: KeyboardListener(
+          autofocus: true,
+          focusNode: _focus,
+          onKeyEvent: (KeyEvent event) {
+            final key = event.logicalKey;
+            if (key != LogicalKeyboardKey.controlLeft && key != LogicalKeyboardKey.controlRight) {
+              return;
+            }
+            if (event is KeyDownEvent) isCtrlPressed.value = true;
+            if (event is KeyUpEvent) isCtrlPressed.value = false;
+          },
+          child: ValueListenableBuilder(
+            valueListenable: isCtrlPressed,
+            builder: (context, value, _) => ScrollConfiguration(
+              behavior: value ? const ScrollBehaviorNever() : const MaterialScrollBehavior(),
+              child: widget.child,
+            ),
           ),
         ),
       ),
