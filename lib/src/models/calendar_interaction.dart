dcca4e3e31f61e8dc44e33114a11a53a271f221b
Werner Scholtz <werner.scholtz@kdab.com>
2025-04-03 08:19:28 +0200

    Added a todo and documentation improvements.

--- lib/src/models/calendar_interaction.dart
+++ lib/src/models/calendar_interaction.dart
@@ -57,6 +57,10 @@ class CalendarInteraction {
   final bool allowEventCreation;
   static const defaultAllowEventCreation = true;
 
+  /// The gesture used to create an event.
+  /// 
+  /// This gesture determines how users can create new events in the calendar.
+  /// It can be either a tap or a long press gesture.
   final CreateEventGesture createEventGesture;
   static const defaultCreateEventGesture = CreateEventGesture.tap;
   static const defaultMobileCreateEventGesture = CreateEventGesture.longPress;
--- lib/src/platform.dart
+++ lib/src/platform.dart
@@ -1,5 +1,6 @@
 import 'package:universal_platform/universal_platform.dart';
 
+/// TODO: Remove this and use https://api.flutter.dev/flutter/foundation/defaultTargetPlatform.html ?
 /// Check if the current platform is a mobile device.
 ///
 /// Using the [UniversalPlatform] package,to cater for web and other platforms.
