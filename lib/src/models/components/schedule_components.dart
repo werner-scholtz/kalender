77b1a52c4e1fc2ef872e272b53be4f1cf2c56ae8
Werner Scholtz <werner.scholtz@kdab.com>
2025-05-26 08:23:02 +0200

    Updated docs

--- lib/src/models/components/schedule_components.dart
+++ lib/src/models/components/schedule_components.dart
@@ -1,6 +1,6 @@
 import 'package:kalender/src/widgets/components/schedule_date.dart';
 
-/// A class containing custom widget builders for the [MonthBody] and [MonthHeader].
+/// A class containing custom widget builders for the [ScheduleBody`].
 class ScheduleComponents<T extends Object?> {
   /// A function that builds the day header widget.
   final ScheduleDateBuilder dayHeaderBuilder;
