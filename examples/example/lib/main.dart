9113e0023c6718dd0a4bdd5cd7cc122c71e21cf2
Werner Scholtz <werner.scholtz@kdab.com>
2025-06-30 10:30:04 +0200

    Added intl package to kalender.
    - a Locale can now be passed into the CalenderWidget for displaying day / month names.

--- examples/example/lib/main.dart
+++ examples/example/lib/main.dart
@@ -205,10 +205,10 @@ class _MyHomePageState extends State<MyHomePage> {
                       // we need to check the second week of the visibleDateTimeRange to determine the month and year.
                       final secondWeek = value.start.addDays(7);
                       year = secondWeek.year;
-                      month = secondWeek.monthNameEnglish;
+                      month = secondWeek.monthNameLocalized();
                     } else {
                       year = value.start.year;
-                      month = value.start.monthNameEnglish;
+                      month = value.start.monthNameLocalized();
                     }
                     return FilledButton.tonal(
                       onPressed: () {},
