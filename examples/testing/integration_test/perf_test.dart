4e532735dae90c65f853ed300f8c1c2091209e1d
Werner Scholtz <werner.scholtz@kdab.com>
2025-07-18 11:04:22 +0200

    fix

--- examples/testing/integration_test/perf_test.dart
+++ examples/testing/integration_test/perf_test.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:kalender/kalender.dart';
-import 'package:perf_profiling/main.dart';
+import 'package:testing/main.dart';
 
 enum ReportKeys { loadingEvents, navigation, scrolling }
 
