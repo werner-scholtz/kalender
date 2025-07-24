fd00e7bf6371aa0487163cdc78f6913d5e5624b1
Werner Scholtz <werner.scholtz@kdab.com>
2025-07-24 12:16:37 +0200

    Reintroduce universal_platform

--- lib/src/platform.dart
+++ lib/src/platform.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/foundation.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 /// Check if the current platform is a mobile device.
-bool get isMobileDevice {
-  defaultTargetPlatform;
-  return defaultTargetPlatform == TargetPlatform.android || defaultTargetPlatform == TargetPlatform.iOS;
-}
+///
+/// Using the [UniversalPlatform] package,to cater for web and other platforms.
+bool get isMobileDevice => UniversalPlatform.isAndroid || UniversalPlatform.isIOS;
