ac0b242e171bb0733998c1d2d013825094c3bcb7
Werner Scholtz <werner.scholtz@kdab.com>
2025-08-19 10:52:00 +0200

    Added new typedefs for new callbacks.

--- lib/src/models/calendar_callbacks.dart
+++ lib/src/models/calendar_callbacks.dart
@@ -42,15 +42,23 @@ class CalendarCallbacks<T extends Object?> {
   /// The callback for when the calendar page is changed.
   final OnPageChanged? onPageChanged;
 
+  /// TODO: Check how these interact with the [Draggable] and [LongPressDraggable]s.
+
   /// The callback for when a user taps on the calendar.
   final OnTapped? onTapped;
-  final Function? onTappedWithDetails;
 
-  final Function? onLongPressed;
-  final Function? onLongPressedWithDetails;
+  /// The callback for when a user taps on the calendar with details.
+  ///
+  /// The details can be a [DayDetail] or a [MultiDayDetail], depending on the calendar view.
+  final OnTappedWithDetails? onTappedWithDetails;
+
+  /// The callback for when a user long presses on the calendar with details.
+  ///
+  /// The details can be a [DayDetail] or a [MultiDayDetail], depending on the calendar view.
+  final OnLongPressedWithDetails? onLongPressedWithDetails;
 
   /// The callback for when a user taps on a multi-day calendar.
-  @Deprecated('Use onTapped or onLongPressed instead. ')
+  @Deprecated('Use onTapped or onLongPressed instead.')
   final OnMultiDayTapped? onMultiDayTapped;
 
   /// Creates a set of callbacks for the [CalendarView].
@@ -64,7 +72,6 @@ class CalendarCallbacks<T extends Object?> {
     this.onPageChanged,
     this.onTapped,
     this.onTappedWithDetails,
-    this.onLongPressed,
     this.onLongPressedWithDetails,
     this.onMultiDayTapped,
     this.disableEventGestures = false,
@@ -111,6 +118,56 @@ typedef OnEventTappedWithDetail<T extends Object?> = void Function(
   TapDetail detail,
 );
 
+/// The callback for when an event is about to be changed.
+typedef OnEventChange<T extends Object?> = void Function(CalendarEvent<T> event);
+
+/// The callback for when an event is changed.
+///
+/// [event] is the original event.
+/// [updatedEvent] is the updated event.
+typedef OnEventChanged<T extends Object?> = void Function(CalendarEvent<T> event, CalendarEvent<T> updatedEvent);
+
+/// The call back for creating a new event.
+///
+/// [event] is the event that will be created.
+typedef OnEventCreate<T extends Object?> = CalendarEvent<T>? Function(CalendarEvent<T> event);
+
+/// The callback for a new event has been created.
+///
+/// [event] is the event that was created.
+typedef OnEventCreated<T extends Object?> = void Function(CalendarEvent<T> event);
+
+/// The callback for when a calendar page is changed.
+///
+/// [dateTimeRange] is the range of dates that can be displayed in the new page.
+typedef OnPageChanged = void Function(DateTimeRange dateTimeRange);
+
+/// The callback for when a user taps on an empty space in the calendar.
+///
+/// [date] is the DateTime that was tapped. 
+/// If you need more details, use [CalendarCallbacks.onTappedWithDetails].
+typedef OnTapped = void Function(DateTime date);
+
+/// The callback for when a user taps on an empty space in the calendar with details.
+///
+/// [date] is the DateTime that was tapped.
+/// [detail] is the details of the date that was tapped.
+/// - The [detail] can be a [DayDetail] or a [MultiDayDetail].
+typedef OnTappedWithDetails = void Function(DateTime date, TapDetail detail);
+
+/// The callback for when a user long presses on an empty space in the calendar.
+///
+/// [date] is the DateTime that was long pressed.
+/// If you need more details, use [CalendarCallbacks.onLongPressedWithDetails].
+typedef OnLongPressed = void Function(DateTime date);
+
+/// The callback for when a user long presses on an empty space in the calendar with details.
+///
+/// [date] is the DateTime that was long pressed.
+/// [detail] is the details of the date that was long pressed.
+/// - The [detail] can be a [DayDetail] or a [MultiDayDetail].
+typedef OnLongPressedWithDetails = void Function(DateTime date, TapDetail detail);
+
 abstract class TapDetail {
   const TapDetail();
 
@@ -139,35 +196,7 @@ class MultiDayDetail extends TapDetail {
   const MultiDayDetail(this.dateTimeRange);
 }
 
-/// The callback for when an event is about to be changed.
-typedef OnEventChange<T extends Object?> = void Function(CalendarEvent<T> event);
-
-/// The callback for when an event is changed.
-///
-/// [event] is the original event.
-/// [updatedEvent] is the updated event.
-typedef OnEventChanged<T extends Object?> = void Function(CalendarEvent<T> event, CalendarEvent<T> updatedEvent);
-
-/// The call back for creating a new event.
-///
-/// [event] is the event that will be created.
-typedef OnEventCreate<T extends Object?> = CalendarEvent<T>? Function(CalendarEvent<T> event);
-
-/// The callback for a new event has been created.
-///
-/// [event] is the event that was created.
-typedef OnEventCreated<T extends Object?> = void Function(CalendarEvent<T> event);
-
-/// The callback for when a calendar page is changed.
-///
-/// [dateTimeRange] is the range of dates that can be displayed in the new page.
-typedef OnPageChanged = void Function(DateTimeRange dateTimeRange);
-
-/// The callback for when a user taps on an empty space in day.
-///
-/// [date] is the DateTime that was tapped.
-typedef OnTapped = void Function(DateTime date);
-
+/// TODO: Depricate in 1.0.0
 /// The callback for when a user taps on an empty space in a multi-day calendar.
 ///
 /// [dateRange] is the range of dates that was tapped.
