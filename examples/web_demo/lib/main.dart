2d15e62210ed0f344261887934504d7cdd7a378e
Werner Scholtz <werner.scholtz@kdab.com>
2026-03-24 17:29:11 +0200

    Add disclaimer to web demo UI.

--- examples/web_demo/lib/main.dart
+++ examples/web_demo/lib/main.dart
@@ -182,6 +182,22 @@ class _DesktopHomePageState extends State<DesktopHomePage> {
           const LocaleDropdown(),
           const SizedBox(width: 4),
           ViewTypePicker(type: _type, onChanged: (value) => setState(() => _type = value)),
+          IconButton(
+            onPressed: () => showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: const Text('This is a pre-release example for v0.16.0'),
+                content: const Text(
+                  "This is a pre-release example for v0.16.0 which is not yet released, see 'main-wip' branch.",
+                ),
+                actions: [
+                  TextButton(onPressed: () => Navigator.pop(context), child: const Text('OK')),
+                ],
+              ),
+            ),
+            icon: Icon(Icons.warning, color: Theme.of(context).colorScheme.error),
+            tooltip: 'Warning - v0.16.0 pre-release example.',
+          ),
         ],
       ),
       body: EventOverlayPortal(
