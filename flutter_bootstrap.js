8753ed4cafae456e82dcd3b2e1c0251721622b95
werner-scholtz <werner-scholtz@users.noreply.github.com>
2026-09-01 07:53:23 +0000

    deploy: 117f3100262cd2e702f1cf8c55071ad2a8411501

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2981844699" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2339784429" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
